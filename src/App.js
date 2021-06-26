2d9808a5f5e2e6f9c517523575ca88361993b284
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-06-26 12:56:48 +0700

    React hooks: 08 - Pagination với useEffect hooks (2020) �

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import { v4 as uuidv4 } from "uuid";
-import TodoList from "./components/TodoList/index";
-import TodoForm from "./components/TodoForm/index";
+import queryString from "query-string";
+// import TodoList from "./components/TodoList/index";
+// import TodoForm from "./components/TodoForm/index";
+import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 1, title: "I love Easy Frontend! 😍 " },
-    { id: 2, title: "We love Easy Frontend! 🥰 " },
-    { id: 3, title: "They love Easy Frontend! 🚀 " },
+    { id: uuidv4(), title: "I love Easy Frontend! 😍 " },
+    { id: uuidv4(), title: "We love Easy Frontend! 🥰 " },
+    { id: uuidv4(), title: "They love Easy Frontend! 🚀 " },
   ]);
+
+  const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  });
+
+  useEffect(() => {
+    async function fetchPostList() {
+      // ...
+      try {
+        const paramsString = queryString.stringify(filters);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        const { data, pagination } = responseJSON;
+
+        setPostList(data);
+        setPagination(pagination);
+      } catch (error) {
+        console.error("Failed to dfetch post list: ", error);
+      }
+    }
+    fetchPostList();
+  }, [filters]);
+
+  function handlePageChange(newPage) {
+    console.log(newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    });
+  }
 
   function handleTodoClick(todo) {
     console.log(todo);
@@ -34,10 +76,13 @@ function App() {
 
   return (
     <div className="app">
-      <h1>React hooks - TodoList</h1>
+      <h1>React hooks - PostList</h1>
 
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+      <PostList posts={postList} />
+      <Pagination pagination={pagination} onPageChange={handlePageChange} />
     </div>
   );
 }
